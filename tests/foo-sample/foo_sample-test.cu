a6dc4b75bd85988faa7cb1e1d5df797e863e5024
Rommel <rommel@blazingdb.com>
2018-09-04 15:04:56 -0500

    Foo test now is working..

--- tests/foo-sample/foo_sample-test.cu
+++ tests/foo-sample/foo_sample-test.cu
@@ -5,8 +5,8 @@
 #include <thrust/device_vector.h>
 
 #include "gtest/gtest.h"
-#include <gdf/gdf.h>
-#include <gdf/cffi/functions.h>
+/*#include <gdf/gdf.h>
+#include <gdf/cffi/functions.h>*/
 
 TEST(gdf_foo_sample_TEST, case1) {
     //TODO

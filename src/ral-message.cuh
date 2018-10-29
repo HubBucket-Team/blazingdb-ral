f3be82eeafccc432012bfd1755ac0ff5432fa109
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-10-29 12:46:34 -0500

    Don't clone the ipc data

--- src/ral-message.cuh
+++ src/ral-message.cuh
@@ -50,12 +50,9 @@ std::tuple<std::vector<std::vector<gdf_column_cpp>>,
     for(auto column : table.columns) {
     	const std::string column_name = table.columnNames.at(column_index);
     	gdf_column_cpp col = gdf_column_cpp(libgdf::CudaIpcMemHandlerFrom(column.data), (gdf_valid_type*)libgdf::CudaIpcMemHandlerFrom(column.valid), (::gdf_dtype)column.dtype, (size_t)column.size, (gdf_size_type)column.null_count, column_name);
-
-    	gdf_column_cpp clone_gdf = col.clone();
-
-    	handles.push_back( clone_gdf.data());
-    	handles.push_back((void *) clone_gdf.valid());
-    	input_table.push_back(clone_gdf);
+    	handles.push_back(col.data());
+    	handles.push_back((void *) col.valid());
+    	input_table.push_back(col);
 
     	++column_index;
     }

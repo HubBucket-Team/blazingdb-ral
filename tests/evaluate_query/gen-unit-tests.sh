3b40f8596d1f0344797b3bc97fba0934a33a0ae6
Unknown <aocsa.cs@gmail.com>
2018-11-02 12:04:12 -0500

    [testdata-generator] update get-unit-tests generator

--- tests/evaluate_query/gen-unit-tests.sh
+++ tests/evaluate_query/gen-unit-tests.sh
@@ -3,6 +3,8 @@ CWD="$(pwd)"
 echo "Build cpp library"
 cd ../../resources/testdata-generator
 
-python3 testdata.py sample.json ~/blazingdb/blazingdb-calcite/blazingdb-calcite-cli/target/BlazingCalciteCli.jar -O ../../tests/evaluate_query/evaluate_query.cu
+#example: ~/blazingdb/blazingdb-calcite/blazingdb-calcite-cli/target/BlazingCalciteCli.jar
+CalciteCli=$1
+python3 testdata.py sample.json $CalciteCli -O ../../tests/evaluate_query/evaluate_query.cu
 cd $CWD
 clang-format-3.8 -i -style=Google evaluate_query.cu

4056c2e69e136d0fd00821449549d03c7ece53ef
Unknown <aocsa.cs@gmail.com>
2018-11-07 11:33:04 -0500

    [develop] update drill generator

--- resources/tpch-generator/input_generator.py
+++ resources/tpch-generator/input_generator.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 from collections import OrderedDict
 import tpch
 import numpy as np
+import pandas as pd
 import re
 import argparse
 import sys
@@ -51,6 +52,8 @@ def get_reference_input(drill, root_path, test_name, query):
           'column_types': get_column_types(table),
           })
   drill_query = get_drill_query(query)
+
+  print('\t#drill_query:   ', drill_query)
   return ('''
   {
       "testName": "%(test_name)s",
@@ -58,23 +61,25 @@ def get_reference_input(drill, root_path, test_name, query):
       "tables": [%(table_inputs)s],
       "result":  %(result)s,
       "resultTypes": %(result_types)s
+      "resultColumnNames": %(result_names)s
   }
   ''') % {'test_name': test_name,
           'query': get_blazingsql_query(db_name, query),
           'table_inputs': ','.join(table_inputs),
           'result': get_reference_result(drill, table, drill_query),
           'result_types': get_reference_result_types(drill, table, drill_query),
+          'result_names': get_reference_result_names(drill, table, drill_query),
           }
 
+def get_reference_result_names(drill, table, query_str):
+  query_result = drill.query(query_str)
+  return  '[%s]' % (','.join(['"%s"' % name for name in query_result.columns]))
 
 def get_reference_result(drill, table, query_str):
   query_result = drill.query(query_str)
   df = query_result.to_dataframe()
-  b1 = table.keys()
-  b2 = list(df.columns.values)
-  b3 = [val for val in b1 if val in b2]
   items = []
-  for column in b3:
+  for column in query_result.columns:
     s = '[%s]' % (','.join([item for item in np.asarray(df[column])]))
     items.append(s)
 
@@ -82,11 +87,26 @@ def get_reference_result(drill, table, query_str):
 
 def get_reference_result_types(drill, table, query_str):
   query_result = drill.query(query_str)
-  data_frame = query_result.to_dataframe()
-  b1 = table.keys()
-  b2 = list(data_frame.columns.values)
-  b3 = [val for val in b1 if val in b2]
-  return '[%s]' % (','.join([ '"%s"' % get_gdf_type(table, name) for name in b3]))
+  df = query_result.to_dataframe()
+  items = []
+  def get_dtype(dtype):
+    if pd.api.types.is_categorical_dtype(dtype):
+      return 'GDF_INT8'
+    dicc = {np.float64: 'GDF_FLOAT64',
+               np.float32: 'GDF_FLOAT32',
+               np.int64:   'GDF_INT64',
+               np.int32:   'GDF_INT32',
+               np.int16:   'GDF_INT16',
+               np.int8:    'GDF_INT8',
+               np.bool_:   'GDF_INT8',
+               np.datetime64: 'GDF_DATE64',
+               np.object: 'GDF_INT64'
+               }
+    _type = np.dtype(dtype).type
+    if _type in dicc :
+      return dicc[_type]
+    return 'GDF_INT64'
+  return '[%s]' % (','.join([ '"%s"' % get_dtype(df[column].dtype) for column in query_result.columns]))
 
 def get_gdf_type(table, column_name):
   types = {
@@ -154,13 +174,47 @@ if __name__ == '__main__':
 
   tpch_path = args.tpch_path
   tpch.init_schema(drill, tpch_path)
-  queries = [
-    '''select c_custkey, c_nationkey, c_acctbal from customer where c_custkey < 15''',
-    '''select c_custkey, c_nationkey, c_acctbal from customer where c_custkey < 150 and c_nationkey = 5''',
+
+  aggregration_queries = [
+    'select count(p_partkey), sum(p_partkey), avg(p_partkey), max(p_partkey), min(p_partkey) from part'
+  ]
+
+  groupby_queries = [
+    'select count(c_custkey), sum(c_acctbal), avg(c_acctbal), min(c_custkey), max(c_nationkey), c_nationkey from customer group by c_nationkey',
+    'select count(c_custkey), sum(c_acctbal), avg(c_acctbal), min(c_custkey), max(c_custkey), c_nationkey from customer where c_custkey < 50 group by c_nationkey',
+    # 'select count(c_custkey) + sum(c_acctbal) + avg(c_acctbal), min(c_custkey) - max(c_nationkey), c_nationkey * 2 as key from customer where key < 40 group by key'
+  ]
+  orderby_queries = [
+    'select c_custkey, c_acctbal from customer order by c_acctbal',
+    'select c_custkey, c_nationkey, c_acctbal from customer order by c_acctbal',
+    'select c_custkey, c_nationkey, c_acctbal from customer order by c_nationkey, c_acctbal',
+    'select c_custkey, c_nationkey, c_acctbal from customer order by c_nationkey, c_custkey'
+  ]
+  join_queries = [
+    'select nation.n_nationkey, region.r_regionkey from nation inner join region on region.r_regionkey = nation.n_nationkey',
+    'select avg(c.c_custkey), avg(c.c_nationkey), n.n_regionkey from customer as c inner join nation as n on c.c_nationkey = n.n_nationkey group by n.n_regionkey',
+    'select c.c_custkey, c.c_nationkey, n.n_regionkey from customer as c inner join nation as n on c.c_nationkey = n.n_nationkey where n.n_regionkey = 1 and c.c_custkey < 50',
+    'select avg(c.c_custkey), avg(c.c_acctbal), n.n_nationkey, r.r_regionkey from customer as c inner join nation as n on c.c_nationkey = n.n_nationkey inner join region as r on r.r_regionkey = n.n_regionkey group by n.n_nationkey, r.r_regionkey',
+    'select n1.n_nationkey as supp_nation, n2.n_nationkey as cust_nation, l.l_extendedprice * l.l_discount from supplier as s inner join lineitem as l on s.s_suppkey = l.l_suppkey inner join orders as o on o.o_orderkey = l.l_orderkey inner join customer as c on c.c_custkey = o.o_custkey inner join nation as n1 on s.s_nationkey = n1.n_nationkey inner join nation as n2 on c.c_nationkey = n2.n_nationkey where n1.n_nationkey = 1 and n2.n_nationkey = 2 and o.o_orderkey < 10000',
+  ]
+  where_queries = [
+    'select c_custkey, c_nationkey, c_acctbal from customer where c_custkey < 15',
+    'select c_custkey, c_nationkey, c_acctbal from customer where c_custkey < 150 and c_nationkey = 5',
+    'select c_custkey, c_nationkey as nkey from customer where c_custkey < 0',
+    'select c_custkey, c_nationkey as nkey from customer where c_custkey < 0 and c_nationkey >=30',
+    'select c_custkey, c_nationkey as nkey from customer where c_custkey < 0 or c_nationkey >= 24',
+    'select c_custkey, c_nationkey as nkey from customer where c_custkey < 0 and c_nationkey >= 3',
+    'select c_custkey, c_nationkey as nkey from customer where -c_nationkey + c_acctbal > 750.3',
+    'select c_custkey, c_nationkey as nkey from customer where -c_nationkey + c_acctbal > 750'
+    ]
+
+  issues_query = [
+    'select count(c_custkey), c_nationkey, count(c_acctbal) from customer group by c_nationkey'
     ]
 
   json_list = []
-  for index, query in enumerate(queries):
+  for index, query in enumerate(where_queries):
+    print('## processing...\t', query)
     json_text = get_reference_input(drill, tpch_path, 'TEST_0%s' % index , query)
     json_list.append(json_text)
 

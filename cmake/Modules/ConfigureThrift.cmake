630dab5bc8a9f99bd4ff36a33e413416a994c6e6
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-12-06 11:15:32 -0500

    release mode for thirft

--- cmake/Modules/ConfigureThrift.cmake
+++ cmake/Modules/ConfigureThrift.cmake
@@ -18,7 +18,7 @@
 # BEGIN macros
 
 macro(CONFIGURE_THRIFT_EXTERNAL_PROJECT)
-    set(THRIFT_CMAKE_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+    set(THRIFT_CMAKE_ARGS "-DCMAKE_BUILD_TYPE=Release"
                         "-DCMAKE_CXX_FLAGS=${EP_CXX_FLAGS}"
                         "-DCMAKE_C_FLAGS=${EP_C_FLAGS}"
                         "-DBUILD_SHARED_LIBS=OFF"

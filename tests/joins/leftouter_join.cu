90fe69be0db58c0c248a39590d549080d47ca068
Rommel <rommel@blazingdb.com>
2018-12-06 10:48:21 -0500

    [leftouter-join] Fixed left-outer join tests

--- tests/joins/leftouter_join.cu
+++ tests/joins/leftouter_join.cu
@@ -49,12 +49,8 @@ TEST_F(EvaluateQueryTest, TEST_00) {
       .logicalPlan =
           "LogicalProject(n_nationkey=[$0], r_regionkey=[$4])\n  "
           "LogicalFilter(condition=[<($0, 10)])\n    "
-          "LogicalProject(n_nationkey=[$0], n_name=[$1], n_regionkey=[$2], "
-          "n_comment=[$3], r_regionkey=[$5], r_name=[$6], r_comment=[$7])\n    "
-          "  LogicalJoin(condition=[=($4, $5)], joinType=[left])\n        "
-          "LogicalProject(n_nationkey=[$0], n_name=[$1], n_regionkey=[$2], "
-          "n_comment=[$3], n_nationkey0=[CAST($0):BIGINT NOT NULL])\n          "
-          "EnumerableTableScan(table=[[main, nation]])\n        "
+          "LogicalJoin(condition=[=($0, $4)], joinType=[left])\n      "
+          "EnumerableTableScan(table=[[main, nation]])\n      "
           "EnumerableTableScan(table=[[main, region]])",
       .tableGroup =
           LiteralTableGroupBuilder{
@@ -73,7 +69,7 @@ TEST_F(EvaluateQueryTest, TEST_00) {
                  Literals<GDF_INT64>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}}},
               {"main.region",
-               {{"r_regionkey", Literals<GDF_INT64>{0, 0, 0, 0, 0}},
+               {{"r_regionkey", Literals<GDF_INT32>{0, 1, 2, 3, 4}},
                 {"r_name", Literals<GDF_INT64>{0, 0, 0, 0, 0}},
                 {"r_comment", Literals<GDF_INT64>{0, 0, 0, 0, 0}}}}}
               .Build(),
@@ -105,12 +101,8 @@ TEST_F(EvaluateQueryTest, TEST_01) {
       .logicalPlan =
           "LogicalProject(n_nationkey=[$0], r_regionkey=[$4], EXPR$2=[+($0, "
           "$4)])\n  LogicalFilter(condition=[<($0, 10)])\n    "
-          "LogicalProject(n_nationkey=[$0], n_name=[$1], n_regionkey=[$2], "
-          "n_comment=[$3], r_regionkey=[$5], r_name=[$6], r_comment=[$7])\n    "
-          "  LogicalJoin(condition=[=($4, $5)], joinType=[left])\n        "
-          "LogicalProject(n_nationkey=[$0], n_name=[$1], n_regionkey=[$2], "
-          "n_comment=[$3], n_nationkey0=[CAST($0):BIGINT NOT NULL])\n          "
-          "EnumerableTableScan(table=[[main, nation]])\n        "
+          "LogicalJoin(condition=[=($0, $4)], joinType=[left])\n      "
+          "EnumerableTableScan(table=[[main, nation]])\n      "
           "EnumerableTableScan(table=[[main, region]])",
       .tableGroup =
           LiteralTableGroupBuilder{
@@ -129,7 +121,7 @@ TEST_F(EvaluateQueryTest, TEST_01) {
                  Literals<GDF_INT64>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}}},
               {"main.region",
-               {{"r_regionkey", Literals<GDF_INT64>{0, 0, 0, 0, 0}},
+               {{"r_regionkey", Literals<GDF_INT32>{0, 1, 2, 3, 4}},
                 {"r_name", Literals<GDF_INT64>{0, 0, 0, 0, 0}},
                 {"r_comment", Literals<GDF_INT64>{0, 0, 0, 0, 0}}}}}
               .Build(),
@@ -159,72 +151,12 @@ TEST_F(EvaluateQueryTest, TEST_02) {
       .query =
           "select n.n_nationkey, r.r_regionkey from main.nation as n left "
           "outer join main.region as r on n.n_regionkey = r.r_regionkey where "
-          "n.n_nationkey < 10",
-      .logicalPlan =
-          "LogicalProject(n_nationkey=[$0], r_regionkey=[$4])\n  "
-          "LogicalFilter(condition=[<($0, 10)])\n    "
-          "LogicalProject(n_nationkey=[$0], n_name=[$1], n_regionkey=[$2], "
-          "n_comment=[$3], r_regionkey=[$5], r_name=[$6], r_comment=[$7])\n    "
-          "  LogicalJoin(condition=[=($4, $5)], joinType=[left])\n        "
-          "LogicalProject(n_nationkey=[$0], n_name=[$1], n_regionkey=[$2], "
-          "n_comment=[$3], n_regionkey0=[CAST($2):BIGINT NOT NULL])\n          "
-          "EnumerableTableScan(table=[[main, nation]])\n        "
-          "EnumerableTableScan(table=[[main, region]])",
-      .tableGroup =
-          LiteralTableGroupBuilder{
-              {"main.nation",
-               {{"n_nationkey",
-                 Literals<GDF_INT32>{0,  1,  2,  3,  4,  5,  6,  7,  8,
-                                     9,  10, 11, 12, 13, 14, 15, 16, 17,
-                                     18, 19, 20, 21, 22, 23, 24}},
-                {"n_name",
-                 Literals<GDF_INT64>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
-                {"n_regionkey",
-                 Literals<GDF_INT32>{0, 1, 1, 1, 4, 0, 3, 3, 2, 2, 4, 4, 2,
-                                     4, 0, 0, 0, 1, 2, 3, 4, 2, 3, 3, 1}},
-                {"n_comment",
-                 Literals<GDF_INT64>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}}},
-              {"main.region",
-               {{"r_regionkey", Literals<GDF_INT64>{0, 0, 0, 0, 0}},
-                {"r_name", Literals<GDF_INT64>{0, 0, 0, 0, 0}},
-                {"r_comment", Literals<GDF_INT64>{0, 0, 0, 0, 0}}}}}
-              .Build(),
-      .resultTable =
-          LiteralTableBuilder{
-              "ResultSet",
-              {{"GDF_INT64", Literals<GDF_INT64>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
-               {"GDF_INT64",
-                Literals<GDF_INT64>{0, 1, 1, 1, 4, 0, 3, 3, 2, 2}}}}
-              .Build()};
-  auto logical_plan = input.logicalPlan;
-  auto input_tables = input.tableGroup.ToBlazingFrame();
-  auto table_names = input.tableGroup.table_names();
-  auto column_names = input.tableGroup.column_names();
-  std::vector<gdf_column_cpp> outputs;
-  gdf_error err = evaluate_query(input_tables, table_names, column_names,
-                                 logical_plan, outputs);
-  EXPECT_TRUE(err == GDF_SUCCESS);
-  auto output_table =
-      GdfColumnCppsTableBuilder{"output_table", outputs}.Build();
-  CHECK_RESULT(output_table, input.resultTable);
-}
-TEST_F(EvaluateQueryTest, TEST_03) {
-  auto input = InputTestItem{
-      .query =
-          "select n.n_nationkey, r.r_regionkey from main.nation as n left "
-          "outer join main.region as r on n.n_regionkey = r.r_regionkey where "
           "n.n_nationkey < 10 and n.n_nationkey > 5",
       .logicalPlan =
           "LogicalProject(n_nationkey=[$0], r_regionkey=[$4])\n  "
           "LogicalFilter(condition=[AND(<($0, 10), >($0, 5))])\n    "
-          "LogicalProject(n_nationkey=[$0], n_name=[$1], n_regionkey=[$2], "
-          "n_comment=[$3], r_regionkey=[$5], r_name=[$6], r_comment=[$7])\n    "
-          "  LogicalJoin(condition=[=($4, $5)], joinType=[left])\n        "
-          "LogicalProject(n_nationkey=[$0], n_name=[$1], n_regionkey=[$2], "
-          "n_comment=[$3], n_regionkey0=[CAST($2):BIGINT NOT NULL])\n          "
-          "EnumerableTableScan(table=[[main, nation]])\n        "
+          "LogicalJoin(condition=[=($2, $4)], joinType=[left])\n      "
+          "EnumerableTableScan(table=[[main, nation]])\n      "
           "EnumerableTableScan(table=[[main, region]])",
       .tableGroup =
           LiteralTableGroupBuilder{
@@ -243,7 +175,7 @@ TEST_F(EvaluateQueryTest, TEST_03) {
                  Literals<GDF_INT64>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}}},
               {"main.region",
-               {{"r_regionkey", Literals<GDF_INT64>{0, 0, 0, 0, 0}},
+               {{"r_regionkey", Literals<GDF_INT32>{0, 1, 2, 3, 4}},
                 {"r_name", Literals<GDF_INT64>{0, 0, 0, 0, 0}},
                 {"r_comment", Literals<GDF_INT64>{0, 0, 0, 0, 0}}}}}
               .Build(),

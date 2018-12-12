aef93f64247da2ab540acb025602cf4639522ca2
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-12-12 00:31:22 -0500

    Port to old test

--- tests/parquet/file_reader/single_column_file-test.cpp
+++ tests/parquet/file_reader/single_column_file-test.cpp
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2018 BlazingDB, Inc.
  *     Copyright 2018 Cristhian Alberto Gonzales Castillo <cristhian@blazingdb.com>
@@ -15,126 +16,107 @@
  * limitations under the License.
  */
 
-#include <sstream>
-
-#include <boost/filesystem.hpp>
-
 #include <arrow/io/file.h>
 #include <arrow/util/logging.h>
-
-#include <parquet/properties.h>
-#include <parquet/schema.h>
-#include <parquet/types.h>
+#include <parquet/api/reader.h>
+#include <parquet/api/writer.h>
 
 #include <gtest/gtest.h>
 
-#include "src/parquet/file_reader.h"
 #include "src/parquet/column_reader.h"
+#include "src/parquet/file_reader.h"
 
-#include <cudf.h>
+#ifndef PARQUET_FILE_PATH
+#error PARQUET_FILE_PATH must be defined for precompiling
+#define PARQUET_FILE_PATH "/"
+#endif
 
-
-//file:reader-test.parquet
-//Total RowCount: 50000
-
-/* NOTE Meta info for file:reader-test.parquet
-file:          file:reader-test.parquet 
-creator:       parquet-cpp version 1.4.0 
-
-file schema:   schema 
---------------------------------------------------------------------------------
-boolean_field: REQUIRED BOOLEAN R:0 D:0
-int64_field:   REQUIRED INT64 R:0 D:0
-double_field:  REQUIRED DOUBLE R:0 D:0
-
-row group 1:   RC:50000 TS:767525 OFFSET:4 
---------------------------------------------------------------------------------
-boolean_field:  BOOLEAN SNAPPY DO:0 FPO:4 SZ:329/6281/19.09 VC:50000 ENC:PLAIN,RLE
-int64_field:    INT64 SNAPPY DO:393 FPO:354318 SZ:454082/500168/1.10 VC:50000 ENC:PLAIN,RLE,PLAIN_DICTIONARY
-double_field:   DOUBLE SNAPPY DO:454570 FPO:667527 SZ:313114/500168/1.60 VC:50000 ENC:PLAIN,RLE,PLAIN_DICTIONARY
-*/
-
-/* NOTE Schema for file:reader-test.parquet
-message schema {
-  required boolean boolean_field;
-  required int64 int64_field;
-  required double double_field;
+inline static void
+checkMetadata(const std::shared_ptr<const ::parquet::FileMetaData> &metadata) {
+    EXPECT_EQ(1, metadata->num_row_groups());
+    EXPECT_EQ(3, metadata->num_columns());
 }
 
-*/
+inline static void
+checkRowGroup(const std::unique_ptr<gdf::parquet::FileReader> &reader) {
+    const std::shared_ptr<::parquet::RowGroupReader> row_group =
+      reader->RowGroup(0);
 
-class SingleColumnFileTest : public testing::Test {
-protected:
-    SingleColumnFileTest();
+    std::size_t  i;
+    std::int16_t definition_level;
+    std::int16_t repetition_level;
+    std::uint8_t valid_bits;
+    std::int64_t levels_read;
+    std::int64_t values_read = 0;
+    std::int64_t nulls_count;
 
-    virtual void SetUp() override;
-    virtual void TearDown() override;
+    std::shared_ptr<parquet::ColumnReader> column;
 
-    static constexpr std::size_t kRowsPerGroup = 100;
-
-    std::string filename;
-};
-
-using Types = ::testing::Types<::parquet::BooleanType,
-                               ::parquet::Int32Type,
-                               ::parquet::Int64Type,
-                               ::parquet::FloatType,
-                               ::parquet::DoubleType>;
-
-TYPED_TEST_CASE(SingleColumnFileTest, Types);
-
-SingleColumnFileTest::SingleColumnFileTest() {
-}
-
-void SingleColumnFileTest::SetUp() {
-    filename = std::string(PARQUET_FILE_PATH);
+    column = row_group->Column(0);
+    gdf::parquet::BoolReader *bool_reader = static_cast<gdf::parquet::BoolReader *>(column.get());
+    i = 0;
     
-    std::cout << "READING PARQUET FILE: " << filename << std::endl;
+//    while (bool_reader->HasNext()) {
+//        bool value;
+//        bool_reader->ReadBatchSpaced(1,
+//                                     &definition_level,
+//                                     &repetition_level,
+//                                     &value,
+//                                     &valid_bits,
+//                                     0,
+//                                     &levels_read,
+//                                     &values_read,
+//                                     &nulls_count);
+//        bool expected = (i % 2) == 0;
+//        EXPECT_EQ(expected, value);
+//        i++;
+//    }
+
+    column = row_group->Column(1);
+    gdf::parquet::Int64Reader *int64_reader =
+      static_cast<gdf::parquet::Int64Reader *>(column.get());
+    i = 0;
+//    while (int64_reader->HasNext()) {
+//        std::int64_t value;
+//        int64_reader->ReadBatchSpaced(1,
+//                                      &definition_level,
+//                                      &repetition_level,
+//                                      &value,
+//                                      &valid_bits,
+//                                      0,
+//                                      &levels_read,
+//                                      &values_read,
+//                                      &nulls_count);
+//        std::int64_t expected = static_cast<std::int64_t>(i) * 1000000000000;
+//        EXPECT_EQ(expected, value);
+//        i++;
+//    }
+
+    column = row_group->Column(2);
+    gdf::parquet::DoubleReader *double_reader =
+      static_cast<gdf::parquet::DoubleReader *>(column.get());
+    i = 0;
+//    while (double_reader->HasNext()) {
+//        double value;
+//        double_reader->ReadBatchSpaced(1,
+//                                       &definition_level,
+//                                       &repetition_level,
+//                                       &value,
+//                                       &valid_bits,
+//                                       0,
+//                                       &levels_read,
+//                                       &values_read,
+//                                       &nulls_count);
+//        double expected = i * 0.001;
+//        EXPECT_EQ(expected, value);
+//        i++;
+//    }
 }
 
-void SingleColumnFileTest::TearDown() {
-}
-
-TEST_F(SingleColumnFileTest, ReadAll) {
+TEST(FileReaderTest, Read) {
     std::unique_ptr<gdf::parquet::FileReader> reader =
-      gdf::parquet::FileReader::OpenFile(this->filename);
+      gdf::parquet::FileReader::OpenFile(PARQUET_FILE_PATH);
 
-    std::shared_ptr<gdf::parquet::ColumnReader<parquet::DoubleType>> column_reader =
-      std::static_pointer_cast<gdf::parquet::ColumnReader<parquet::DoubleType>>(
-        reader->RowGroup(0)->Column(0));
-
-    ASSERT_TRUE(column_reader->HasNext());
-
-    const std::size_t rowsPerGroup = this->kRowsPerGroup;
-
-    gdf_column column{
-      .data       = new std::uint8_t[rowsPerGroup * sizeof(double)],
-      .valid      = new std::uint8_t[rowsPerGroup],
-      .size       = 0,
-      .dtype      = GDF_invalid,
-      .null_count = 0,
-      .dtype_info = {},
-    };
-
-    std::int16_t definition_levels[rowsPerGroup];
-    std::int16_t repetition_levels[rowsPerGroup];
-
-    const std::size_t total_read = column_reader->ToGdfColumn(column);
-
-    EXPECT_EQ(rowsPerGroup, total_read);
-
-    double val = 0.001;
-    const double h = 0.001;
-
-    for (std::size_t i = 0; i < rowsPerGroup; i++) {
-        double expected = val;
-        std::int64_t value = static_cast<double *>(column.data)[i];
-       
-        EXPECT_EQ(expected, value);
-        
-        val += h;
-    }
-
-    delete[] static_cast<std::uint8_t *>(column.data);
-    delete[] column.valid;
+    checkMetadata(reader->metadata());
+    checkRowGroup(reader);
 }

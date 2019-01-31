4b73dce458bcbdc746b081c70a40127c10b52481
jeanp413 <jeanp413@hotmail.com>
2019-01-31 11:02:50 -0500

    Updated jit and interops benchmarks

--- benchmarks/interops/interops_benchmark.cpp
+++ benchmarks/interops/interops_benchmark.cpp
@@ -29,40 +29,44 @@
 
 using namespace gdf::library;
 
-template <int logPlan>
+static std::vector<std::string> LOGICAL_PLANS = {
+  "LogicalProject(EXPR$0=[+($0, $2)])",
+  "LogicalProject(EXPR$0=[*(SIN(/(+($0, $2), $1)), POWER(/(-($0, $2), $1), 2))])",
+  "LogicalProject(EXPR$1=[-(FLOOR(+(POWER(SIN($1), 2), POWER(COS($1), 2))), CEIL(+(POWER(SIN(+($1, 53.42)), 2), POWER(COS(+($1, 53.42)), 2))))])",
+  "LogicalProject(EXPR$0=[*(*(10, SIN(/(+($0, $2), $1))), POWER(/(-($0, $2), $1), 2))], EXPR$1=[-(FLOOR(+(POWER(SIN($1), 2), POWER(COS($1), 2))), CEIL(+(POWER(SIN(+($1, 53.42)), 2), POWER(COS(+($1, 53.42)), 2))))], EXPR$2=[+(-(/(*(FLOOR(+($1, 0.1)), POWER(MOD($0, 13), 2.0)), 5), CEIL(MOD(*(2, $2), 57))), 0.001)])"
+};
+
+static void CustomArguments(benchmark::internal::Benchmark* b) {
+  for (int i = 0; i < LOGICAL_PLANS.size(); ++i)
+    for (int64_t j = 50 << 10; j <= 80 << 20; j *= 6)
+      b->Args({i, j});
+}
+
+template <int logPlanIndex>
 struct InterOpsBench : public benchmark::Fixture {
  public:
-  void SetUp(benchmark::State& state) override {
-    logicalPlan = "LogicalProject(EXPR$0=[+($0, $2)])";
-    logicalPlan = "LogicalProject(EXPR$0=[*(*(10, SIN(/(+($0, $2), $1))), POWER(/(-($0, $2), $1), 2))], EXPR$1=[-(FLOOR(+(POWER(SIN($1), 2), POWER(COS($1), 2))), CEIL(+(POWER(SIN(+($1, 53.42)), 2), POWER(COS(+($1, 53.42)), 2))))], EXPR$2=[+(-(/(*(FLOOR(+($1, 0.1)), POWER(MOD($0, 13), 2.0)), 5), CEIL(MOD(*(2, $2), 57))), 0.001)])";
-  }
+  void SetUp(benchmark::State& state) override {}
 
   void TearDown(benchmark::State& state) override {}
 
   std::string logicalPlan;
 };
 
-// namespace {
-// char const* LOGICAL_PLANS[] = {"LogicalProject(EXPR$0=[+($0, $2)])"};
-// }
-
-// template <char const* logPlan>
-// struct BenchParameters {
-//   static constexpr char const* logicalPlan{logPlan};
-// };
 
 BENCHMARK_TEMPLATE_DEFINE_F(InterOpsBench, SimpleBench, 4)
 (benchmark::State& state) {
+  logicalPlan = LOGICAL_PLANS[state.range(0)];
+  
   std::vector<int32_t> x;
   std::vector<double> y;
-  std::vector<int64_t> z;
-
-  x.resize(state.range(0));
+  std::vector<int32_t> z;
+  x.resize(state.range(1));
   std::generate(x.begin(), x.end(),
                 []() { return std::rand() % (RAND_MAX / 13); });
-  y.resize(state.range(0));
-  std::generate(y.begin(), y.end(), []() { return ((std::rand() % RAND_MAX) + 1); });
-  z.resize(state.range(0));
+  y.resize(state.range(1));
+  std::generate(y.begin(), y.end(),
+                []() { return ((std::rand() % RAND_MAX) + 1); });
+  z.resize(state.range(1));
   std::generate(z.begin(), z.end(),
                 []() { return std::rand() % (RAND_MAX / 2); });
 
@@ -70,12 +74,11 @@ BENCHMARK_TEMPLATE_DEFINE_F(InterOpsBench, SimpleBench, 4)
       LiteralTableGroupBuilder{{"temp",
                                 {{"in_x", Literals<GDF_INT32>{std::move(x)}},
                                  {"in_y", Literals<GDF_FLOAT64>{std::move(y)}},
-                                 {"in_z", Literals<GDF_INT64>{std::move(z)}}}}}
+                                 {"in_z", Literals<GDF_INT32>{std::move(z)}}}}}
           .Build();
   auto input_tables = tableGroup.ToBlazingFrame();
 
   blazing_frame bz_frame, bz_out;
-  for (auto& t : input_tables) bz_frame.add_table(t);
 
   gdf_error err = GDF_SUCCESS;
   for (auto _ : state) {
@@ -83,10 +86,8 @@ BENCHMARK_TEMPLATE_DEFINE_F(InterOpsBench, SimpleBench, 4)
     bz_frame.clear();
     for (auto& t : input_tables) bz_frame.add_table(t);
     state.ResumeTiming();
-
+    
     auto params = parse_project_plan(bz_frame, logicalPlan);
-
-    // perform operations
     if (params.num_expressions_out <= 0) {
       state.SkipWithError("num_expressions_out is not good!");
       break;
@@ -104,5 +105,5 @@ BENCHMARK_TEMPLATE_DEFINE_F(InterOpsBench, SimpleBench, 4)
 }
 
 BENCHMARK_REGISTER_F(InterOpsBench, SimpleBench)
-    ->Range(50 << 10, 50 << 20)
+    ->Apply(CustomArguments)
     ->Unit(benchmark::kMillisecond);

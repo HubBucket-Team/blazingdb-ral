78c4a49c3e432fbfc8e71fd3b94c186e3e415250
Christian Noboa Mardini <christian@blazingdb.com>
2019-02-14 15:22:41 -0500

    Revert "Fixed some issues in gdf_column_cpp class and added the unit tests for those cases."
    This reverts commit d021d5e98d521e7d78db27177092cf4a670dc613.

--- src/GDFCounter.cu
+++ src/GDFCounter.cu
@@ -135,11 +135,3 @@ GDFRefCounter* GDFRefCounter::getInstance()
         Instance=new GDFRefCounter();
     return Instance;
 }
-
-std::size_t GDFRefCounter::column_ref_value(gdf_column* column) {
-    auto it = map.find(column);
-    if (it == map.end()) {
-        return 0;
-    }
-    return it->second;
-}

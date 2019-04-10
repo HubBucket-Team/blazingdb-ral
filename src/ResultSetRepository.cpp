3cf920d6a7dd85b8f6fd4fd0e0ecea335bb9f1bf
Rommel <rommel@blazingdb.com>
2019-04-10 08:18:28 -0500

    [custrings-support] fixed column name issue that was causing a crash on the third running

--- src/ResultSetRepository.cpp
+++ src/ResultSetRepository.cpp
@@ -77,10 +77,10 @@ void result_set_repository::update_token(query_token_t token, blazing_frame fram
 			new_gdf_column->null_count = frame.get_column(i).null_count();
 			new_gdf_column->data = (void * ) new_strings;
 			new_gdf_column->dtype = frame.get_column(i).dtype();
+			new_gdf_column->col_name = const_cast<char*>(frame.get_column(i).name().c_str());
 
 			gdf_column_cpp string_column;
 			string_column.create_gdf_column(new_gdf_column);
-			string_column.set_name(frame.get_column(i).name());
 			string_column.get_gdf_column()->dtype = GDF_STRING; //TODO create_gdf_column no soporta GDF_STRING porque type_dispatcher tampoco lo soporta
 																//esto significa que allocated_size_data tecnicamente esta incorrecto
 			frame.set_column(i,string_column);

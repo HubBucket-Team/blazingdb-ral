a19fb092bfb689527df36d9f5a123dd57ed2bf19
Alexander <aocsa.cs@gmail.com>
2019-05-07 16:48:17 -0500

    [ucx] fix ipc of ipc error

--- src/testing-libgdf.cu
+++ src/testing-libgdf.cu
@@ -595,7 +595,7 @@ auto  interpreterServices(const blazingdb::protocol::Buffer &requestPayloadBuffe
 }
 
 
-int main(int argc, const char *argv[])
+int main(int argc, const char *argv2[])
 {
 
   // #ifndef VERBOSE
@@ -605,16 +605,20 @@ int main(int argc, const char *argv[])
     std::cout << "RAL Engine starting" << std::endl;
 
     std::string identifier {"1"};
-    if (argc == 2) {
-        identifier = std::string(argv[1]);
-    }
-    if (argc > 1 && argc != 6) {
-      std::cout << "Usage: " << argv[0]
-                << " <RAL_ID>"
-                   " <ORCHESTRATOR_[IP|HOSTNAME]> <ORCHESTRATOR_PORT>"
-                   " <RAL_[IP|HOSTNAME]> <RAL_PORT>\n";
-      return 1;
-    }
+    // if (argc == 2) {
+    //     identifier = std::string(argv[1]);
+    // }
+    // if (argc > 1 && argc != 6) {
+    //   std::cout << "Usage: " << argv[0]
+    //             << " <RAL_ID>"
+    //                " <ORCHESTRATOR_[IP|HOSTNAME]> <ORCHESTRATOR_PORT>"
+    //                " <RAL_[IP|HOSTNAME]> <RAL_PORT>\n";
+    //   return 1;
+    // }
+
+    argc = 6;
+    const char * argv[] = {"./testing-libgdf",  "2", "192.168.1.61",  "9000", "192.168.1.61",  "8988"};
+   
 
     if (argc == 6) {
       identifier = std::string(argv[1]);

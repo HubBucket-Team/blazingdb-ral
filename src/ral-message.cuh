f3757b83bb1c8994336acf96bd2aebf6d8e4ad84
William Malpica <william@blazingdb.com>
2019-06-06 18:12:48 -0500

    trying to reduce usage of protocol stuff in Schema

--- src/ral-message.cuh
+++ src/ral-message.cuh
@@ -60,68 +60,68 @@ static void* CudaIpcMemHandlerFrom (const std::basic_string<int8_t>& handler) {
   return response;
 }
 
-std::tuple<std::vector<std::vector<gdf_column_cpp>>,
-           std::vector<std::string>,
-           std::vector<std::vector<std::string>>> toBlazingDataframe(uint64_t accessToken, const ::blazingdb::protocol::TableGroupDTO& request,std::vector<void *> & handles)
-{
-  std::vector<std::vector<gdf_column_cpp>> input_tables;
-  std::vector<std::string> table_names;
-  std::vector<std::vector<std::string>> column_names;
+// std::tuple<std::vector<std::vector<gdf_column_cpp>>,
+//            std::vector<std::string>,
+//            std::vector<std::vector<std::string>>> toBlazingDataframe(uint64_t accessToken, const ::blazingdb::protocol::TableGroupDTO& request,std::vector<void *> & handles)
+// {
+//   std::vector<std::vector<gdf_column_cpp>> input_tables;
+//   std::vector<std::string> table_names;
+//   std::vector<std::vector<std::string>> column_names;
 
-  for(auto table : request.tables) {
-    //table_names.push_back(table.name);
-    //column_names.push_back(table.columnNames);
+//   for(auto table : request.tables) {
+//     //table_names.push_back(table.name);
+//     //column_names.push_back(table.columnNames);
 
-    std::vector<gdf_column_cpp> input_table;
+//     std::vector<gdf_column_cpp> input_table;
 
-    int column_index = 0;
-    for(auto column : table.columns) {
+//     int column_index = 0;
+//     for(auto column : table.columns) {
 
-      gdf_column_cpp col;
+//       gdf_column_cpp col;
 
-      if (table.columnTokens[column_index] == 0){
-        //const std::string column_name = table.columnNames.at(column_index);
-        const std::string column_name = "";
+//       if (table.columnTokens[column_index] == 0){
+//         //const std::string column_name = table.columnNames.at(column_index);
+//         const std::string column_name = "";
         
-        if((::gdf_dtype)column.dtype == GDF_STRING){
+//         if((::gdf_dtype)column.dtype == GDF_STRING){
 
-          nvstrings_ipc_transfer ipc;  // NOTE: IPC handles will be closed when nvstrings_ipc_transfer goes out of scope
-          memcpy(&ipc,column.custrings_data.data(),sizeof(nvstrings_ipc_transfer));
+//           nvstrings_ipc_transfer ipc;  // NOTE: IPC handles will be closed when nvstrings_ipc_transfer goes out of scope
+//           memcpy(&ipc,column.custrings_data.data(),sizeof(nvstrings_ipc_transfer));
 
-          NVStrings* strs = NVStrings::create_from_ipc(ipc);
-          NVCategory* category = NVCategory::create_from_strings(*strs);
-          NVStrings::destroy(strs);
+//           NVStrings* strs = NVStrings::create_from_ipc(ipc);
+//           NVCategory* category = NVCategory::create_from_strings(*strs);
+//           NVStrings::destroy(strs);
 
-          col.create_gdf_column(category, column.size, column_name);
+//           col.create_gdf_column(category, column.size, column_name);
 
-        } else {
-          if ((::gdf_dtype)column.dtype == GDF_STRING_CATEGORY) 
-            std::cout<<"WARNING: incoming data is a GDF_STRING_CATEGORY"<<std::endl;
+//         } else {
+//           if ((::gdf_dtype)column.dtype == GDF_STRING_CATEGORY) 
+//             std::cout<<"WARNING: incoming data is a GDF_STRING_CATEGORY"<<std::endl;
 
-          // col.create_gdf_column_for_ipc((::gdf_dtype)column.dtype,libgdf::CudaIpcMemHandlerFrom(column.data),(gdf_valid_type*)libgdf::CudaIpcMemHandlerFrom(column.valid),column.size,column_name);
-          void * dataHandle = libgdf::CudaIpcMemHandlerFrom(column.data);
-          void * validHandle = libgdf::CudaIpcMemHandlerFrom(column.valid);
-          col.create_gdf_column_for_ipc((::gdf_dtype)column.dtype,dataHandle,
-                                          static_cast<gdf_valid_type*>(validHandle), column.size, column.null_count, column_name);
-          handles.push_back(dataHandle);
-          if (validHandle != nullptr){
-            handles.push_back(validHandle);
-          }
-        }
+//           // col.create_gdf_column_for_ipc((::gdf_dtype)column.dtype,libgdf::CudaIpcMemHandlerFrom(column.data),(gdf_valid_type*)libgdf::CudaIpcMemHandlerFrom(column.valid),column.size,column_name);
+//           void * dataHandle = libgdf::CudaIpcMemHandlerFrom(column.data);
+//           void * validHandle = libgdf::CudaIpcMemHandlerFrom(column.valid);
+//           col.create_gdf_column_for_ipc((::gdf_dtype)column.dtype,dataHandle,
+//                                           static_cast<gdf_valid_type*>(validHandle), column.size, column.null_count, column_name);
+//           handles.push_back(dataHandle);
+//           if (validHandle != nullptr){
+//             handles.push_back(validHandle);
+//           }
+//         }
 
-      }else{
-        col = result_set_repository::get_instance().get_column(accessToken, table.columnTokens[column_index]);
-      }
+//       }else{
+//         col = result_set_repository::get_instance().get_column(accessToken, table.columnTokens[column_index]);
+//       }
 
-      input_table.push_back(col);
+//       input_table.push_back(col);
 
-      ++column_index;
-    }
+//       ++column_index;
+//     }
 
-    input_tables.push_back(input_table);
-  }
+//     input_tables.push_back(input_table);
+//   }
 
-  return std::make_tuple(input_tables, table_names, column_names);
-}
+//   return std::make_tuple(input_tables, table_names, column_names);
+// }
 
 } //namespace libgdf

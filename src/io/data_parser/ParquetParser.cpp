b8f59ec7b66551b0e9f2a733abef9787677a389c
William Malpica <william@blazingdb.com>
2019-06-12 09:20:30 -0500

    first draft of new parquet parse_schema using apache parquet. DOES NOT COMPILE

--- src/io/data_parser/ParquetParser.cpp
+++ src/io/data_parser/ParquetParser.cpp
@@ -8,18 +8,24 @@
 #include "ParquetParser.h"
 #include "cudf/io_functions.hpp"
 
-#include <cuio/parquet/api.h>
+// #include <cuio/parquet/api.h>
 
 #include <arrow/io/file.h>
-#include <arrow/util/logging.h>
+// #include <arrow/util/logging.h>
 
-#include <boost/filesystem.hpp>
+// #include <boost/filesystem.hpp>
 
-#include <parquet/column_writer.h>
-#include <parquet/file_writer.h>
-#include <parquet/properties.h>
+// #include <parquet/column_writer.h>
+// #include <parquet/file_writer.h>
+// #include <parquet/properties.h>
+// #include <parquet/schema.h>
+// #include <parquet/types.h>
+
+
+#include <parquet/file_reader.h>
+#include <parquet/reader.h>
 #include <parquet/schema.h>
-#include <parquet/types.h>
+
 
 #include <GDFColumn.cuh>
 #include <GDFCounter.cuh>
@@ -100,9 +106,90 @@ void parquet_parser::parse(std::shared_ptr<arrow::io::RandomAccessFile> file,
 	}
 }
 
+// This function is copied and adapted from cudf
+constexpr std::pair<gdf_dtype, gdf_dtype_extra_info> to_dtype(
+    parquet::Type::type physical, parquet::LogicalType::type logical) {
+
+	bool strings_to_categorical = false; // parameter used in cudf::read_parquet
+
+  // Logical type used for actual data interpretation; the legacy converted type
+  // is superceded by 'logical' type whenever available.
+  switch (logical) {
+    case parquet::LogicalType::type::UINT_8:
+    case parquet::LogicalType::type::INT_8:
+      return std::make_pair(GDF_INT8, gdf_dtype_extra_info{TIME_UNIT_NONE});
+    case parquet::LogicalType::type::UINT_16:
+    case parquet::LogicalType::type::INT_16:
+      return std::make_pair(GDF_INT16, gdf_dtype_extra_info{TIME_UNIT_NONE});
+    case parquet::LogicalType::type::DATE:
+      return std::make_pair(GDF_DATE32, gdf_dtype_extra_info{TIME_UNIT_NONE});
+    case parquet::LogicalType::type::TIMESTAMP_MILLIS:
+      return std::make_pair(GDF_DATE64, gdf_dtype_extra_info{TIME_UNIT_ms});
+    case parquet::LogicalType::type::TIMESTAMP_MICROS:
+      return std::make_pair(GDF_DATE64, gdf_dtype_extra_info{TIME_UNIT_us});
+    default:
+      break;
+  }
+
+  // Physical storage type supported by Parquet; controls the on-disk storage
+  // format in combination with the encoding type.
+  switch (physical) {
+    case parquet::Type::type::BOOLEAN:
+      return std::make_pair(GDF_BOOL8, gdf_dtype_extra_info{TIME_UNIT_NONE});
+    case parquet::Type::type::INT32:
+      return std::make_pair(GDF_INT32, gdf_dtype_extra_info{TIME_UNIT_NONE});
+    case parquet::Type::type::INT64:
+      return std::make_pair(GDF_INT64, gdf_dtype_extra_info{TIME_UNIT_NONE});
+    case parquet::Type::type::FLOAT:
+      return std::make_pair(GDF_FLOAT32, gdf_dtype_extra_info{TIME_UNIT_NONE});
+    case parquet::Type::type::DOUBLE:
+      return std::make_pair(GDF_FLOAT64, gdf_dtype_extra_info{TIME_UNIT_NONE});
+    case parquet::Type::type::BYTE_ARRAY:
+    case parquet::Type::type::FIXED_LEN_BYTE_ARRAY:
+      // Can be mapped to GDF_CATEGORY (32-bit hash) or GDF_STRING (nvstring)
+      return std::make_pair(strings_to_categorical ? GDF_CATEGORY : GDF_STRING,
+                            gdf_dtype_extra_info{TIME_UNIT_NONE});
+    case parquet::Type::type::INT96:
+      // Convert Spark INT96 timestamp to GDF_DATE64
+      return std::make_pair(GDF_DATE64, gdf_dtype_extra_info{TIME_UNIT_ms});
+    default:
+      break;
+  }
+
+  return std::make_pair(GDF_invalid, gdf_dtype_extra_info{TIME_UNIT_NONE});
+}
+
 void parquet_parser::parse_schema(std::vector<std::shared_ptr<arrow::io::RandomAccessFile> > files, ral::io::Schema & schema)  {
 
-	gdf_error error = gdf::parquet::read_schema(files, schema);
+	// gdf_error error = gdf::parquet::read_schema(files, schema);
+	std::vector<std::string> column_names;
+	std::vector<gdf_dtype> dtypes;
+	std::vector<size_t> num_row_groups(files.size());
+	
+	std::unique_ptr<parquet::ParquetFileReader> parquet_reader = parquet::ParquetFileReader::Open(files[0]);
+	std::shared_ptr<parquet::FileMetaData> file_metadata = parquet_reader->metadata();
+	int total_columns = file_metadata->num_columns();
+	num_row_groups[0] = file_metadata->num_row_groups();
+	parquet::SchemaDescriptor * schema = file_metadata->schema();
+	for(int i = 0; i < total_columns; i++){
+		const parquet::ColumnDescriptor * column = schema->Column(i);
+		column_names.push_back(StringUtil::toLower(column->name()));
+		std::pair<gdf_dtype, gdf_dtype_extra_info> dtype_pair = to_dtype(column->physical_type(), column->logical_type());
+		dtypes.push_back(dtype_pair.first);
+	}
+	parquet_reader->Close();
+
+	for(int file_index = 1; file_index < files.size(); file_index++){
+		parquet_reader = parquet::ParquetFileReader::Open(files[file_index]);
+		file_metadata = parquet_reader->metadata();
+		schema = file_metadata->schema();
+		num_row_groups[file_index] = file_metadata->num_row_groups();
+		parquet_reader->Close();
+	}
+
+	std::vector<std::size_t> column_indices(total_columns);
+    std::iota(column_indices.begin(), column_indices.end(), 0);
+	schema_out = ral::io::Schema(column_names,column_indices,dtypes,num_row_groups);
 }
 
 } /* namespace io */
